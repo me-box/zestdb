079113435d061816787c2559904af560bb601d44
John Moore <jmoore@zedstar.org>
2017-11-06 12:49:13 +0000

    build changes

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ echo "compiling..."
 eval `opam config env`
 cd src && jbuilder build server.exe
 cp base-cat.json ../
+cp example-server-key ../
+cp example-token-key ../
 cp ./_build/default/server.exe ../
 cd ../test && jbuilder build client.exe
 cp ./_build/default/client.exe ../
 cd ../utils && jbuilder build mint.exe
 cp ./_build/default/mint.exe ../
-# setup a key for demo purposes
-echo -n 'EKy(xjAnIfg6AT+OGd?nS1Mi5zZ&b*VXA@WxNLLE' > /tmp/key
 echo "done compiling"
